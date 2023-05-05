d15016a1703a2cd249298d157184796084765b89
LucyMacko <118434024+LucyMacko@users.noreply.github.com>
2023-05-05 08:54:50 +0000

    Final version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "A turtle", "The burgler"];
+  let action = ["destroyed", "chewed", "tore", "cut"];
+  let what = ["my homework", "the bills", "the cake "];
+  let when = [
+    "before the class",
+    "after school",
+    "when I finished",
+    "during my lunch",
+    "yesterday"
+  ];
+  return (
+    who[Math.floor(Math.random() * action.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
